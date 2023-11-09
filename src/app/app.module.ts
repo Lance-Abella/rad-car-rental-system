cf49ec8873be47540ae9e4239e8c0a4ab3a50740
Lance Mathew Abella <abellalance2@gmail.com>
2023-11-09 18:30:43 +0800

    fixing the issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent,
     NavigationBarComponent
   ],
   imports: [
