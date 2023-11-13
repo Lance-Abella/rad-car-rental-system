c07dc2c684ae927b0c947ed69d48a8bffe6e5248
Harley Mamalias <harleymamalias@gmail.com>
2023-11-13 22:23:59 +0800

    implemented initial routing and few changes in login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsForRentComponent } from './cars-for-rent/cars-for-rent.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'cars-for-rent', component: CarsForRentComponent}
+  {path: 'cars-for-rent', component: CarsForRentComponent},
+  {path: 'loginpage', component: LoginpageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CarsForRentComponent } from './cars-for-rent/cars-for-rent.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,13 @@ import { CarsForRentComponent } from './cars-for-rent/cars-for-rent.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
