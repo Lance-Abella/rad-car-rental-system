791488b05103e1366ddcbfaed46f68e4cfdd67c8
Lance Mathew Abella <abellalance2@gmail.com>
2023-11-16 22:49:02 +0800

    rented vehicles page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserpageComponent } from './userpage/userpage.component';
 import { CarPageComponent } from './car-page/car-page.component';
+import { RentedCarPageComponent } from './rented-car-page/rented-car-page.component';
 
 const routes: Routes = [
   {path:'', component:UserpageComponent},
   {path:'search/:searchTerm', component:UserpageComponent},
   {path:'tag/:tag', component:UserpageComponent},
   {path:'car/:id', component:CarPageComponent},
+  {path:'rented', component:RentedCarPageComponent},
+  {path:'car', component:CarPageComponent},
+  {path:'userpage', component:UserpageComponent},
 ];
 
 @NgModule({
--- src/app/rented-car-page/rented-car-page.component.ts
+++ src/app/rented-car-page/rented-car-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Car } from '../shared/models/car';
+import { CarService } from '../services/car/car.service';
 
 @Component({
   selector: 'app-rented-car-page',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./rented-car-page.component.scss']
 })
 export class RentedCarPageComponent {
+  cars:Car[]= [];
+  constructor(private carService: CarService){
+    
+  }
 
+  ngOnInit(): void{
+  this.cars = this.carService.getAll();
+  }
 }
